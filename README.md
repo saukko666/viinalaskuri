4cb5cddf4cb3341841d10fb36e8cdbe0967a39c0
Sauli <>
2021-01-26 22:15:11 +0200

    first content

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Viinalaskuri
 
-This app calculates booze usage and optionally prices
+This app calculates booze total volume and total prices from user input
+
+## todo
+
+basically everything, including this readme
+
+## issues
+
+subtotal does not update to reduced state yet
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,127 @@
-import React from "react";
+import React, { useReducer } from "react";
+import {
+  Paper,
+  Button,
+  Typography,
+  TextField,
+  Container,
+  Input,
+} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      padding: theme.spacing(1),
+      margin: theme.spacing(1),
+    },
+  })
+);
 
 function App() {
-  return <div></div>;
+  const classes = useStyles();
+  type FormData = {
+    item: string;
+    qty: number | string;
+    price: number | string;
+    subtotal: string;
+  };
+  var formData: FormData = {
+    item: "",
+    qty: "",
+    price: "",
+    subtotal: "",
+  };
+
+  // thanks to https://medium.com/javascript-in-plain-english/react-controlled-forms-with-hooks-538762aab935
+  const reducer = (
+    state: FormData,
+    { field, value }: { [x: string]: string | number }
+  ) => {
+    return {
+      ...state,
+      [field]: value,
+    };
+  };
+  const [form, dispatch] = useReducer(reducer, formData);
+  const handleSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+  };
+  const handleForm = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    let target = evt.currentTarget;
+    let field = target.id;
+
+    /**
+     *  Needs more checks but this should be enough for now.
+     */
+    if (field in form) {
+      if (target.type === "number" && !!!isNaN(target.valueAsNumber)) {
+        dispatch({
+          field: field,
+          value: target.valueAsNumber,
+        });
+      } else {
+        dispatch({
+          field: field,
+          value: target.value,
+        });
+      }
+    }
+  };
+
+  const { item, qty, price } = form;
+  return (
+    <Container maxWidth="sm">
+      <Paper className={classes.paper}>
+        <Typography component="h1" variant="h4">
+          Viinalaskuri
+        </Typography>
+
+        <form action="#" method="GET" onSubmit={handleSubmit}>
+          <Paper className={classes.paper} style={{ display: "flex" }}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+              <TextField
+                placeholder="Item name"
+                id="item"
+                value={item}
+                onChange={handleForm}
+              ></TextField>
+              <TextField
+                placeholder="Quantity"
+                id="qty"
+                value={qty}
+                type="number"
+                onChange={handleForm}
+              ></TextField>
+              <TextField
+                placeholder="Price per quantity"
+                id="price"
+                value={price}
+                type="number"
+                inputProps={{ step: 0.1 }}
+                onChange={handleForm}
+              ></TextField>
+              <Input
+                placeholder="Subtotal"
+                readOnly
+                id="subtotal"
+                value={(Number(qty) * Number(price)).toFixed(2)}
+                type="text"
+              ></Input>
+            </div>
+          </Paper>
+          <br />
+          <Button disabled type="submit" color="primary" variant="contained">
+            Submit
+          </Button>
+        </form>
+
+        <Paper className={classes.paper}>
+          <pre>{JSON.stringify(form, null, 2)}</pre>
+        </Paper>
+      </Paper>
+    </Container>
+  );
 }
 
 export default App;
